f326970e5d8e9e3c4daff8b267b69cd501aeded1
izumi.tokumura <izunt.n@gmail.om>
2019-11-03 10:24:57 +0000

    favposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,15 @@ class User < ApplicationRecord
 
   has_many :microposts
   
+  #フォロー
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  
+  #お気に入り
+  has_many :favorites
+  has_many :favposts, through: :favorites, source: :micropost
   
   
   def follow(other_user)
@@ -31,5 +36,20 @@ class User < ApplicationRecord
   
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
+  end
+  
+
+ #お気に入り
+  def like(micropost)
+    favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+
+  def unlike(micropost)
+    favorite = favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+
+  def  favpost?(micropost)
+    self.favposts.include?(micropost)
   end
 end
